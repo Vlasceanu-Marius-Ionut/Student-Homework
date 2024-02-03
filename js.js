3a26fcfe9ab9ea280c46e9f5534be823f592c63b
Vlasceanu Marius <vlasceanu_marius117@yahoo.com>
2024-02-03 19:13:19 +0200

    firstCommit

---
+++ js.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+const goTo1 = document.getElementById('img1');
+const goTo2 = document.getElementById('img2')
+const goTo3 = document.getElementById('img3')
+
+goTo1.addEventListener('click', () =>{
+    window.location.href = 'https://ro.wikipedia.org/wiki/Octavianus_Augustus'
+})
+goTo2.addEventListener('click',() =>{
+    window.location.href = 'https://ro.wikipedia.org/wiki/Hadrian'
+})
+goTo3.addEventListener('click', () =>{
+    window.location.href = 'https://ro.wikipedia.org/wiki/Marc_Aureliu'
+})
